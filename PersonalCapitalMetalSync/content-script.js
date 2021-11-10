de63641ba16419abf20c65d4bbf2ad7b87109f00
DerekZhang0000 <45522063+DerekZhang0000@users.noreply.github.com>
2021-11-09 23:46:22 -0600

    Add files via upload

--- PersonalCapitalMetalSync/content-script.js
+++ PersonalCapitalMetalSync/content-script.js
@@ -1,4 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-    alert("fdsfdsafdsa");
-    console.log("gdsagdsagd");
-});
+fetch('https://www.apmex.com/silver-price')
+    .then(function(response) {
+        return response.text()
+    })
+    .then(function(html) {
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(html, "text/html");
+        var silverPrice = parseFloat(doc.getElementsByClassName("price").item(4).innerHTML.replace('$', ''));
+        console.log(silverPrice);
+    });
